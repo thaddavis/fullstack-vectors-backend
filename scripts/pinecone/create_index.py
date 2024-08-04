68bd0b1bd6a316c5497e9f992af2373f57154626
tad dy <a@a.com>
2024-08-04 17:13:10 -0400

    adding useful comment

--- scripts/pinecone/create_index.py
+++ scripts/pinecone/create_index.py
@@ -6,7 +6,7 @@ load_dotenv()
 
 api_key = os.getenv("PINECONE_API_KEY")
 index_name = os.getenv("PINECONE_INDEX")
-embedding_dimensions = 384
+embedding_dimensions = 384 # Depends on the embedding model you are using
 pc = Pinecone(api_key=api_key)
 
 pc.create_index(

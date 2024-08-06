72e19a66b69560ca4c02add77ebe7cda36ca5560
tad dy <a@a.com>
2024-08-06 17:13:30 -0400

    remove comment

--- src/routers/auth/router.py
+++ src/routers/auth/router.py
@@ -59,7 +59,6 @@ async def create_user(db: db_dependency, create_user_request: UserCreateRequest)
 @router.post('/token', response_model=Token)
 async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
                                  db: db_dependency):
-    print('*** *** ***')
     user = authenticate_user(form_data.username, form_data.password, db)
     if not user:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Could not validate user")

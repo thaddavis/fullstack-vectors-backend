23bc1a7f796e7d444fd91b456b006cb29879bcfe
tad dy <a@a.com>
2024-08-10 20:06:18 -0400

    saving after removing routines to simplify the application

--- src/db/models.py
+++ src/db/models.py
@@ -3,12 +3,6 @@ from sqlalchemy.orm import relationship
 from .database import Base
 import datetime
 
-workout_routine_association = Table(
-    'workout_routine', Base.metadata,
-    Column('workout_id', Integer, ForeignKey('workouts.id')),
-    Column('routine_id', Integer, ForeignKey('routines.id'))
-)
-
 class User(Base):
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True, index=True)
@@ -21,16 +15,7 @@ class Workout(Base):
     user_id = Column(Integer, ForeignKey('users.id'))
     name = Column(String, index=True)
     description = Column(String, index=True)
-    routines = relationship('Routine', secondary=workout_routine_association, back_populates='workouts')
     
-class Routine(Base):
-    __tablename__ = 'routines'
-    id = Column(Integer, primary_key=True, index=True)
-    user_id = Column(Integer, ForeignKey('users.id'))
-    name = Column(String, index=True)
-    description = Column(String, index=True)
-    workouts = relationship('Workout', secondary=workout_routine_association, back_populates='routines')
-
 # For storing agent chat_history
 class ChatHistory(Base):
     __tablename__ = 'chat_history'
@@ -38,5 +23,3 @@ class ChatHistory(Base):
     session_id = Column(UUID, nullable=False)
     message = Column(JSON, nullable=False)
     created_at = Column(TIMESTAMP, default=datetime.datetime.now())
-    
-Workout.routines = relationship('Routine', secondary=workout_routine_association, back_populates='workouts')

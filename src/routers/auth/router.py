60483aa898039304db7a964789589da7d539e93e
tad dy <a@a.com>
2024-08-13 15:39:29 -0400

    testing with different domain specified in the http cookie set by the FastAPI

--- src/routers/auth/router.py
+++ src/routers/auth/router.py
@@ -159,7 +159,7 @@ def logout(response: Response):
         secure=True,
         samesite="None",
         path="/",
-        domain="fullstack-rag-nextjs-service-esw7hvt5nq-ue.a.run.app",
+        domain=".a.run.app",
         max_age=0  # Setting max_age to 0 effectively deletes the cookie
     )
     return {"message": "Logged out successfully"}

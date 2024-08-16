0a8e97fceca179c2b3f963aea3b293231df83b4e
tad dy <a@a.com>
2024-08-16 19:00:40 -0400

    change name of tool

--- src/routers/reActAgent/tools/tad_tool.py
+++ src/routers/reActAgent/tools/tad_tool.py
@@ -48,7 +48,7 @@ class SearchQuery(BaseModel):
     pass
 
 tad_tool = StructuredTool(
-    name="search_tad_vectors_tool",
+    name="search_tad_vectors",
     description="A tool for accessing info related Tad Duval.",
     func=search_the_index,
     coroutine=search_the_index,

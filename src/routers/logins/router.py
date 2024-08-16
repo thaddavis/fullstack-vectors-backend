2a223b3d2a26f9bfbd5da0a4cecfb5cf708dfcc4
tad dy <a@a.com>
2024-08-15 20:39:51 -0400

    got the infinite scroll working decent when listing logins

--- src/routers/logins/router.py
+++ src/routers/logins/router.py
@@ -14,5 +14,21 @@ router = APIRouter()
 
 @router.get('/')
 @limiter.limit("10/minute")
-def get_logins(db: db_dependency, jwt: jwt_dependency, request: Request):
-    return db.query(Logins).filter(Logins.account_id == jwt.get('id')).order_by(Logins.created_at.desc()).all()
+def get_logins(db: db_dependency, jwt: jwt_dependency, request: Request, cursor: Optional[int]):
+
+    print('cursor', cursor)
+
+    results = db.query(Logins).filter(Logins.account_id == jwt.get('id')).order_by(Logins.created_at.desc()).offset(cursor).limit(40).all()
+
+    results = [{
+        "id": r.id,
+        'account_id': r.account_id,
+        'ip_address': r.ip_address,
+        'created_at': r.created_at,
+        'similarity_score': r.similarity_score
+    } for r in results]
+
+    return {
+        "results": results,
+        "cursor": cursor + 40
+    }
